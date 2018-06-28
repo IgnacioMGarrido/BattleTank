8b9537e26df8bb2f82757ddc82ef604e7fd4580a
Ignacio Martinez <nama1510@gmail.com>
2018-06-28 16:29:45 +0200

    BT40 2/2 Tracks Fixed

--- BattleTank/Source/BattleTank/Private/TankTracks.cpp
+++ BattleTank/Source/BattleTank/Private/TankTracks.cpp
@@ -14,22 +14,23 @@ void UTankTracks::BeginPlay()
 
 void UTankTracks::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction * ThisTickFunction)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Tank Tracks ticking"))
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
 	//Calculate the slippage speed
 	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
 
 	//Work out the requiered acceleration this frame to correct.
-	auto CorrectionAcceleration = -(SlippageSpeed / DeltaTime * GetRightVector());
+	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
 
 	//Claculate and apply sideways force (F = m * a)
 	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
 	auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2;
-
+	UE_LOG(LogTemp, Warning, TEXT("Correction Force is: %d"), *CorrectionForce.ToString())
 	TankRoot->AddForce(CorrectionForce);
 }
 
-void UTankTracks::SetThrottle(float Throttle) {
+void UTankTracks::SetThrottle(float Throttle) 
+{
 
 	Throttle = FMath::Clamp<float>(Throttle, -1, 1);
 
